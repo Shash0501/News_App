5413c87125bab431105687ad14c0fab857d770b8
shashank <shash.201cs257@gmail.com>
2021-09-04 22:18:59 +0530

    added the web browser view for each news article

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_web_browser/flutter_web_browser.dart';
 import 'package:news_app/blocs/news_bloc/bloc/news_bloc.dart';
 import 'package:news_app/models/articleModel.dart';
 
@@ -64,57 +65,74 @@ class _HomeScreenState extends State<HomeScreen> {
                 return ListView.builder(
                   itemCount: _articlesList.length,
                   itemBuilder: (context, index) {
-                    return Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey,
-                                blurRadius: 1,
-                                offset: Offset(0, 2),
-                                spreadRadius: 1,
+                    return GestureDetector(
+                      onDoubleTap: () {
+                        FlutterWebBrowser.openWebPage(
+                          url: _articlesList[index].url,
+                          customTabsOptions: CustomTabsOptions(
+                            colorScheme: CustomTabsColorScheme.dark,
+                            toolbarColor: Colors.deepPurple,
+                            secondaryToolbarColor: Colors.green,
+                            navigationBarColor: Colors.deepPurple,
+                            addDefaultShareMenuItem: true,
+                            instantAppsEnabled: true,
+                            showTitle: true,
+                            urlBarHidingEnabled: true,
+                          ),
+                        );
+                      },
+                      child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  blurRadius: 1,
+                                  offset: Offset(0, 2),
+                                  spreadRadius: 1,
+                                )
+                              ]),
+                          height: height * 0.15,
+                          margin: EdgeInsets.only(
+                              bottom: height * 0.02,
+                              top: height * 0.02,
+                              left: width * 0.02,
+                              right: width * 0.02),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: width * 0.3,
+                                height: height * 0.15,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(10),
+                                      bottomLeft: Radius.circular(10),
+                                    ),
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                          _articlesList[index].urlToImage),
+                                      fit: BoxFit.cover,
+                                    )),
+                              ),
+                              SizedBox(
+                                width: width * 0.03,
+                              ),
+                              Container(
+                                width: width * 0.6,
+                                height: height * 0.2,
+                                padding: EdgeInsets.symmetric(
+                                    vertical: height * 0.01),
+                                child: Text(_articlesList[index].title,
+                                    overflow: TextOverflow.clip,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    )),
                               )
-                            ]),
-                        height: height * 0.15,
-                        margin: EdgeInsets.only(
-                            bottom: height * 0.02,
-                            top: height * 0.02,
-                            left: width * 0.02,
-                            right: width * 0.02),
-                        child: Row(
-                          children: [
-                            Container(
-                              width: width * 0.3,
-                              height: height * 0.15,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(10),
-                                    bottomLeft: Radius.circular(10),
-                                  ),
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                        _articlesList[index].urlToImage),
-                                    fit: BoxFit.cover,
-                                  )),
-                            ),
-                            SizedBox(
-                              width: width * 0.03,
-                            ),
-                            Container(
-                              width: width * 0.6,
-                              height: height * 0.2,
-                              padding:
-                                  EdgeInsets.symmetric(vertical: height * 0.01),
-                              child: Text(_articlesList[index].title,
-                                  overflow: TextOverflow.clip,
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 16,
-                                  )),
-                            )
-                          ],
-                        ));
+                            ],
+                          )),
+                    );
                   },
                 );
               } else if (state is NewsErrorState) {
